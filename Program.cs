292f532d8d28fc5954912841368202d874af2853
Takahiro KOJIMA <12kojima.takahiro@gmail.com>
2020-08-20 00:26:06 +0900

    let it be

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class FullName
+    {
+        public FullName(string firstName, string lastName) 
+        {
+            FirstName = firstName;
+            LastName = lastName;
+        }
+
+        public string FirstName { get; }
+        public string LastName { get; }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = null;
+            var fullName = new FullName("Takahiro", "Kojima");
+            Console.WriteLine(fullName.FirstName);
+            Console.WriteLine(fullName.LastName);
+        }
+    }
+}
